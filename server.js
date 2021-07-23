29889b3c057c38f73774fa601df770c73a6db636
Old Prego <spymasterluke@gmail.com>
2021-07-23 18:35:48 -0500

    fixey poo?

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,15 +14,15 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(require(path.join(__dirname, "/routes/api.js")));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
